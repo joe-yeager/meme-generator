40dcdead3c0fcd363755f9b929c11d8818d46d00
Joe Yeager <jyeager@hubspot.com>
2023-08-07 17:35:15 -0700

    Update script, add package-lock for serverless.

--- scripts/useLocalVersionOfDevServer.js
+++ scripts/useLocalVersionOfDevServer.js
@@ -6,6 +6,10 @@ const PATH_TO_DEMO_PROJECT = path.join(__dirname, '../src/app/extensions/');
 const PACKAGE_JSON_PATH = path.join(PATH_TO_DEMO_PROJECT, 'package.json');
 const PACKAGE_JSON = require(PACKAGE_JSON_PATH);
 
+if (!PACKAGE_JSON.devDependencies) {
+  PACKAGE_JSON.devDependencies = {};
+}
+
 PACKAGE_JSON.devDependencies['@hubspot/ui-extensions-dev-server'] =
   '../../../../../ui-extensibility/public-packages/ui-extensions-dev-server/';
 
