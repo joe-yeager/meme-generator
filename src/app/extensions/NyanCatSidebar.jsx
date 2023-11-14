8ce46fe9f1bf9a3d1c9006d244aa88afaaedbf8d
Joe Yeager <jyeager@hubspot.com>
2023-11-14 14:14:07 -0800

    Add iframe modal to nyan cat

--- src/app/extensions/NyanCatSidebar.jsx
+++ src/app/extensions/NyanCatSidebar.jsx
@@ -7,17 +7,23 @@ import {
 } from '@hubspot/ui-extensions';
 
 
-hubspot.extend(() => (
-  <NyanCat />
+hubspot.extend(({ actions}) => (
+  <NyanCat actions={actions}/>
 ));
 
 const nyanUrl = "https://64.media.tumblr.com/tumblr_lrbu1l9BJk1qgzxcao1_250.gifv";
 
-const NyanCat =  () => {
+const NyanCat =  ({ actions}) => {
   return (
     <Card>
       <Flex direction="column" align="center">
-        <Image src={nyanUrl} />
+        <Image src={nyanUrl} onClick={() => {
+          actions.openIframeModal({
+            width: 500000,
+            height: 748,
+            uri: "https://www.nyan.cat/"})
+          }}
+        />
       </Flex>
     </Card>);
 };
